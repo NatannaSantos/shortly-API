22c51288801b98656e80b88c5a7c1c4c84575a1e
Natanna <natannacortez@gmail.com>
2022-03-17 20:33:18 -0300

    feat: get urls and correction query database post urls

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -11,9 +11,9 @@ export async function shortenUrl(req,res){
         const newUrl=shortUrl.slice(0,8);
         await connection.query(`
         INSERT INTO 
-        urls(url) 
-        VALUES ($1)
-        `,[newUrl]);
+        urls(url,shortUrl) 
+        VALUES ($1,$2)
+        `,[url.url,newUrl]);
         console.log("shorURL"+newUrl);
         res.status(201).send(`shortUrl:${newUrl}`);
     } catch (error) {
@@ -23,5 +23,29 @@ export async function shortenUrl(req,res){
 }
 
 export async function getUrl(req,res){
+    const {shortUrl}=req.params;
+   
+    try {
+        const {rows:existingUrl}= await connection.query(`
+        SELECT * FROM urls
+        WHERE shortUrl=$1
+        `,[shortUrl]);
+        if (existingUrl.length === 0) {
+            return res.sendStatus(404);
+          }
+       console.log(existingUrl)
+        const returnUrl=existingUrl.map(returnUrl=>({
+            "id":returnUrl.id,
+            "shortUrl":returnUrl.shorturl,
+            "url":returnUrl.url
+        })
+        );
 
+        res.status(200).send(returnUrl);
+
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { shortenUrl}  from "../controllers/urlController.js";
+import { getUrl, shortenUrl}  from "../controllers/urlController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import urlSchema from "../schemas/urlSchema.js";
 
 const urlRouter=Router();
 
 urlRouter.post('/urls/shorten',validateSchemaMiddleware(urlSchema),shortenUrl);
+urlRouter.get('/urls/:shortUrl',getUrl);
 
 export default urlRouter;
